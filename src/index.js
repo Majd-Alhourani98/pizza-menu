bcc4bbeb771aeb46ac2a5a65b2cd11ec13f8996d
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-08-04 23:06:02 +0300

    Build a Pizza component and use it several times within App

--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React</h1>;
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
 }
 
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
+}
+
+const x = {
+  name: "Pizza Spinaci",
+  ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+  price: 12,
+  photoName: "pizzas/spinaci.jpg",
+  soldOut: false,
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
