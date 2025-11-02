655c093fd144f0132ba5b9380f8b5226fe160399
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-02 13:32:29 +0300

    feat: add conditional rendering for open hours and order section in Footer

--- src/index.js
+++ src/index.js
@@ -67,14 +67,18 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
   return (
-    <main className="manu">
-      <h2>our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizza={pizza} key={pizza.name} />
-        ))}
-      </ul>
+    <main className="menu">
+      <h2>Our menu</h2>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
@@ -94,19 +98,18 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 5;
-  const closeHour = 22;
+  const openHour = 1;
+  const closeHour = 7;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-  //   if (isOpen) {
-  //     alert("We're currently open");
-  //   } else {
-  //     alert("Sorry, we're closed");
-  //   }
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}.00. Come visit use or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
 }
