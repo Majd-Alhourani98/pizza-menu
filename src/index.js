c5b17bbb5b2310a908841424fa8a5105e4844f47
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-02 13:23:24 +0300

    feat: render dynamic pizza menu from data array

--- src/index.js
+++ src/index.js
@@ -3,6 +3,51 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
 function App() {
   return (
     <div className="container">
@@ -23,22 +68,27 @@ function Header() {
 
 function Menu() {
   return (
-    <div className="manu">
+    <main className="manu">
       <h2>our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-    </div>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizza={pizza} key={pizza.name} />
+        ))}
+      </ul>
+    </main>
   );
 }
 
-function Pizza() {
+function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, mushrooms, and onion</p>
-    </div>
+    <li className="pizza">
+      <img src={props.pizza.photoName} alt={props.pizza.name} />
+      <div>
+        <h2>{props.pizza.name}</h2>
+        <p>{props.pizza.ingredients}</p>
+        <span>{props.pizza.price}</span>
+      </div>
+    </li>
   );
 }
 
